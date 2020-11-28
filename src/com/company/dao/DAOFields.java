1b4b186d1b3fc821e01b737e207e92f19e3d395f
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2020-11-28 16:57:35 -0300

    DAO.java
    mudanças

--- src/com/company/dao/DAOFields.java
+++ src/com/company/dao/DAOFields.java
@@ -1,5 +1,10 @@
 package com.company.dao;
 
 public interface DAOFields {
-
+    String getTableName();
+    String getDeleteString(String table);
+    String getUpdateString(String table);
+    String getInsertString(String table);
+    String getSelectAllString(String table);
+    String getSelectConditionalString(String table);
 }
--- src/com/company/dao/PersonagemDAO.java
+++ src/com/company/dao/PersonagemDAO.java
@@ -2,5 +2,61 @@ package com.company.dao;
 
 import com.company.model.Personagem;
 
+import java.util.List;
+
 public class PersonagemDAO implements DAO<Personagem>,DAOFields{
+    @Override
+    public List<Personagem> get(String condition) {
+        return null;
+    }
+
+    @Override
+    public List<Personagem> getALL() {
+        return null;
+    }
+
+    @Override
+    public void update(Personagem personagem) {
+
+    }
+
+    @Override
+    public void delete(Personagem personagem) {
+
+    }
+
+    @Override
+    public void create(Personagem personagem) {
+
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public String getDeleteString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getUpdateString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getInsertString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getSelectAllString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getSelectConditionalString(String table) {
+        return null;
+    }
 }
