ea93709a0ce4de4154da72e3afe8e78348b154a6
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-28 17:32:31 -0300

    Class Executor created - Version 0

---
+++ src/com/company/Executor.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+/**
+ * The class where most of the code is ran.
+ *
+ * @version 0
+ * @since 2020-11-28
+ */
+
+/*
+Update 0.:
+- Created class
+- Added initial comment
+- Added update log
+ */
+    
+public class Executor {
+}
