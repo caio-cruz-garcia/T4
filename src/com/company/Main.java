9966ddd8cdc91bcd9d1e4cfd7fd7548d95b178e2
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-29 00:53:04 -0300

    Added authors at Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+/**
+ * Used to run the class "Executor", due to pdf request.
+ * @author Nathan Brito da Silva - 17.00531-0
+ * @author Caio Cruz Alfonso Garcia - 15.01580-7
+ */
 
 public class Main {
     public static void main(String[] args) {
