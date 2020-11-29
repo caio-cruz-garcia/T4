9ecfef9701fbe903412c262d71b13edb87c2346e
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-29 00:59:46 -0300

    class Items.java update 0.1
    Update 0.1.:
    - Added vars
    - Added constructor
    - Added getters and setters

--- src/com/company/model/Items.java
+++ src/com/company/model/Items.java
@@ -2,7 +2,7 @@ package com.company.model;
 
 /**
  * Class items, made to represent an Item that a Character (Personagem) may have.
- * @version 0
+ * @version 0.1
  * @since 2020-11-28
  */
 /*
@@ -10,6 +10,74 @@ Update 0.:
 - Class created
 - Javadoc comment added
 - Log created
+
+Update 0.1.:
+- Added vars
+- Added constructor
+- Added getters and setters
  */
 public class Items {
+    // Vars
+    String name;
+    String desc;
+    int quantity;
+
+    /**
+     * Constructor for items
+     * @param name Name of item
+     * @param desc Desc of item
+     */
+    public Items(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+        quantity = 0;
+    }
+
+    /**
+     * method getName
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * method setName
+     * @param name Set name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * method getDesc
+     * @return desc
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * method setDesc
+     * @param desc Set desc
+     */
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    /**
+     * method getQuantity
+     * @return quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * method setQuantity
+     * @param quantity Set quantity
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
