da6b1b848aafaa18ba6d2d00ad861be864c2c14e
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-29 00:52:09 -0300

    class Items.java created

---
+++ src/com/company/model/Items.java
@@ -0,0 +1,15 @@
+package com.company.model;
+
+/**
+ * Class items, made to represent an Item that a Character (Personagem) may have.
+ * @version 0
+ * @since 2020-11-28
+ */
+/*
+Update 0.:
+- Class created
+- Javadoc comment added
+- Log created
+ */
+public class Items {
+}
