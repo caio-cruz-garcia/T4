dc0ab3f7376efab78f8916030fc0e2b21b95e628
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-30 15:36:46 -0300

    Class EquipmentDAO update 0.11
    Update 0.11.:
    - Added javadoc comments
    - Updated methods getTableName
    - Updated methods getDeleteString
    - Updated methods getUpdateString
    - Updated methods getInsertString
    - Updated methods getSelectAllString
    - Updated methods getSelectConditionalString

--- src/com/company/dao/EquipmentDAO.java
+++ src/com/company/dao/EquipmentDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * class EquipmentDAO. Stores all DAO commands related to Equipment
  *
- * @version 0.1
+ * @version 0.11
  * @since 2020-11-30
  */
 
@@ -25,6 +25,15 @@ Update 0.1.:
 - Added vars
 - Added constructor
 - Implemented methods
+
+Update 0.11.:
+- Added javadoc comments
+- Updated methods getTableName
+- Updated methods getDeleteString
+- Updated methods getUpdateString
+- Updated methods getInsertString
+- Updated methods getSelectAllString
+- Updated methods getSelectConditionalString
  */
 public class EquipmentDAO implements DAO<Equipment>,DAOFields{
     private Connection connection;
@@ -41,58 +50,108 @@ public class EquipmentDAO implements DAO<Equipment>,DAOFields{
         }
     }
 
+    /**
+     * method get
+     * @param condition Special condition for specific queries
+     * @return result
+     */
     @Override
     public List<Equipment> get(String condition) {
         return null;
     }
 
+    /**
+     * method getALL
+     * @return all items from SQL
+     */
     @Override
     public List<Equipment> getALL() {
         return null;
     }
 
+    /**
+     * method update
+     * @param equipment Takes a equipment to update in the table
+     */
     @Override
     public void update(Equipment equipment) {
 
     }
 
+    /**
+     * method delete
+     * @param equipment Deletes an equipment
+     */
     @Override
     public void delete(Equipment equipment) {
 
     }
 
+    /**
+     * method create
+     * @param equipment creates a new equipment entry at the sql database
+     */
     @Override
     public void create(Equipment equipment) {
 
     }
 
+    /**
+     * method getTableName
+     * @return The name of the table
+     */
     @Override
     public String getTableName() {
-        return null;
+        return "equipment";
     }
 
+    /**
+     * method getDeleteString
+     * @param table name of table
+     * @return Delete query
+     */
     @Override
     public String getDeleteString(String table) {
-        return null;
+        return "DELETE FROM "+ table +" WHERE id = ?";
     }
 
+    /**
+     * method getUpdateString
+     * @param table name of table
+     * @return Update query
+     */
     @Override
     public String getUpdateString(String table) {
-        return null;
+        return "UPDATE " + table + " SET id = ?, array = ? WHERE id = ?;";
     }
 
+    /**
+     * method getInsertString
+     * @param table name of table
+     * @return Create query
+     */
     @Override
     public String getInsertString(String table) {
-        return null;
+        return "INSERT INTO " + table + " (id, array) VALUES (?, ?);";
     }
 
+    /**
+     * method getSelectAllString
+     * @param table name of table
+     * @return Return all data query
+     */
     @Override
     public String getSelectAllString(String table) {
-        return null;
+        return "SELECT * FROM " + table;
     }
 
+    /**
+     * method getSelectedConditionalString
+     * @param table name of table
+     * @return return specific search query
+     */
     @Override
     public String getSelectConditionalString(String table) {
-        return null;
+        return "SELECT * FROM " + table + " WHERE ";
     }
 }
