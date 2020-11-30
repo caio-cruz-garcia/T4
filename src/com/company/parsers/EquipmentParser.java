2748b1a01fe5de9bd57ed7ccc6719a7fce084f89
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-30 15:09:13 -0300

    Class EquipmentParser update 0
    Update 0.:
    - Added update log
    - Added javadoc comment
    - Added toJson (Template used)
    - Added fromJson (Template used)

--- src/com/company/parsers/EquipmentParser.java
+++ src/com/company/parsers/EquipmentParser.java
@@ -1,4 +1,48 @@
 package com.company.parsers;
+import com.company.model.Equipment;
+import org.json.JSONObject;
 
+/**
+ * class EquipmentParser, made to parse the JSON data related to Equipment
+ *
+ * @version 0
+ * @since 2020-11-30
+ */
+
+/*
+Update log.:
+Update 0.:
+- Added update log
+- Added javadoc comment
+- Added toJson
+- Added fromJson
+ */
 public class EquipmentParser {
+
+    /**
+     * Converts an Equipment into Json
+     * @param equipment Equipment class
+     * @return Json
+     */
+    public static JSONObject toJson(Equipment equipment){
+        JSONObject json = new JSONObject();
+        json.put("name", equipment.getName());
+        json.put("desc", equipment.getDesc());
+        json.put("stat", equipment.getStat());
+        return json;
+    }
+
+    /**
+     * Converts a Json into Equipment
+     * @param json Json
+     * @return Equipment
+     */
+    public static Equipment fromJson(JSONObject json){
+        Equipment equipment = new Equipment(
+                json.getString("name"),
+                json.getString("desc"),
+                json.getString("stat")
+        );
+        return equipment;
+    }
 }
