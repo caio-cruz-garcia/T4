2aff1b337664c478d44216c96288d157c679f9fc
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-30 15:10:26 -0300

    Class EquipmentParserList update 0
    Update 0.:
    - Added update log
    - Added javadoc comment

--- src/com/company/parsers/EquipmentParserList.java
+++ src/com/company/parsers/EquipmentParserList.java
@@ -1,4 +1,18 @@
 package com.company.parsers;
 
+/**
+ * class EquipmentParserList, made to parse the JSON data related to Equipment as a list
+ *
+ * @version 0
+ * @since 2020-11-30
+ */
+
+/*
+Update log.:
+Update 0.:
+- Added update log
+- Added javadoc comment
+
+ */
 public class EquipmentParserList {
 }
